eb462c02d6f9439d238a9470e02c6fc2487846d5
ChiHouX <nvj335537@163.com>
2024-08-20 21:00:58 +0800

    JWT登录操作完整版

--- src/main/java/com/example/myprojectbackend/entity/RestBean.java
+++ src/main/java/com/example/myprojectbackend/entity/RestBean.java
@@ -5,7 +5,7 @@ import com.alibaba.fastjson2.JSONWriter;
 
 public record RestBean<T>(int code, T data, String message){
     public static <T> RestBean<T> success(T data){
-        return new RestBean<>(200, data, "success");
+        return new RestBean<>(200, data, "请求成功");
     }
     public static <T> RestBean<T> success(){
         return success(null);
